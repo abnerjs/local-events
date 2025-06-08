558e1c5d7284b02d3a3fc7bea80fcde8e287bb80
abnerjs <abner.js05@gmail.com>
2025-06-07 22:15:29 -0300

    feat: navbar preview

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./components/navbar";
+
+function App() {
+	return (
+		<>
+			<header className="flex p-8 w-full">
+				<Navbar />
+			</header>
+		</>
+	);
+}
+
+export default App;
---
+++ app/src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	</StrictMode>,
+);
